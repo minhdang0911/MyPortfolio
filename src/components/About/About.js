312efece8effe02bddbae2dabcf46ffabe5fbc60
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2024-11-05 11:09:47 +0700

    push code

---
+++ src/components/About/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Minh Dang Tran</strong>. I am originally from Ho Chi Minh City, Vietnam, and I graduated from Ho Chi Minh City University of Technology. Through various positions, I have gained valuable experience that has strengthened my work ethic, communication skills, and adaptability
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            My journey into tech started with a passion for problem-solving, which led me to study Computer Systems Technology. At Hutech University, I have learned knowledge related to web installation in school and done projects related to web installation.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development .
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,80 @@
+ 
+export const ProjectList = [
+  {
+    img: "/project1.png",
+    title: "project-e-commerce",
+    description:
+      "Build a e-commerce website with functions such as purchasing orders. Revenue order management",
+    tech_stack: ["React JS", "Cloudinary", "Firebase", "TailwindCSS","ExpressJS","MongoDb"],
+    github_url: "https://github.com/minhdang0911/-project-e-commerce",
+    // demo_url: "https://youknowtheending.net/",
+  },
+  {
+    img: "/project222.png",
+    title: "Booking Care Project",
+    description:
+      "Build a website to book medical appointments with the functions of booking medical appointments for doctors at the clinic",
+    tech_stack: ["React", "MySQl","ExpressJs"],
+    github_url: "https://github.com/minhdang0911/Booking-SERN-fullstack",
+    // demo_url: "https://renr-bcit.web.app/",
+  },
+  {
+    img: "/project3.png",
+    title: "Project Quiz ",
+    description:
+      "Build a website that allows users to answer questions and the admin creates a set of questions for users",
+    tech_stack: ["NextJs","Typescript",""],
+    github_url: "https://github.com/minhdang0911/next-manage-blog",
+    // demo_url: "https://mymindweb.vercel.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+ 
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
